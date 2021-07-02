b7a71ff9e6eecfb559468ae3c8a0e44bdccb3980
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2021-07-02 19:55:41 +0200

    Format code

--- ArrayOAuthDataStore.php
+++ ArrayOAuthDataStore.php
@@ -8,43 +8,50 @@ use IMSGlobal\LTI\OAuth\OAuthToken;
 /**
  * A Trivial memory-based store - no support for tokens
  */
-class ArrayOAuthDataStore extends OAuthDataStore {
+class ArrayOAuthDataStore extends OAuthDataStore
+{
     private $consumers = array();
 
-    function add_consumer($consumer_key, $consumer_secret) {
+    function add_consumer($consumer_key, $consumer_secret)
+    {
         $this->consumers[$consumer_key] = $consumer_secret;
     }
 
-    function lookup_consumer($consumer_key) {
-        if ( strpos($consumer_key, "http://" ) === 0 ) {
-            $consumer = new OAuthConsumer($consumer_key,"secret", null);
+    function lookup_consumer($consumer_key)
+    {
+        if (strpos($consumer_key, "http://") === 0) {
+            $consumer = new OAuthConsumer($consumer_key, "secret", null);
             return $consumer;
         }
-        if ( $this->consumers[$consumer_key] ) {
-            $consumer = new OAuthConsumer($consumer_key,$this->consumers[$consumer_key], null);
+        if ($this->consumers[$consumer_key]) {
+            $consumer = new OAuthConsumer($consumer_key, $this->consumers[$consumer_key], null);
             return $consumer;
         }
         return null;
     }
 
-    function lookup_token($consumer, $token_type, $token) {
+    function lookup_token($consumer, $token_type, $token)
+    {
         return new OAuthToken($consumer, "");
     }
 
     // Return null if the nonce has not been used
     // Return $nonce if the nonce was previously used
-    function lookup_nonce($consumer, $token, $nonce, $timestamp) {
+    function lookup_nonce($consumer, $token, $nonce, $timestamp)
+    {
         // Should add some clever logic to keep nonces from
         // being reused - for no we are really trusting
         // that the timestamp will save us
         return null;
     }
 
-    function new_request_token($consumer) {
+    function new_request_token($consumer)
+    {
         return null;
     }
 
-    function new_access_token($token, $consumer) {
+    function new_access_token($token, $consumer)
+    {
         return null;
     }
 }
