111f5c3b1b69d91fa09f771cb158040d40a10b57
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2021-07-02 20:12:11 +0200

    No need to override methods that return null

--- ArrayOAuthDataStore.php
+++ ArrayOAuthDataStore.php
@@ -28,26 +28,6 @@ class ArrayOAuthDataStore extends OAuthDataStore
 
     function lookup_token($consumer, $token_type, $token)
     {
-        return new OAuthToken($consumer, "");
-    }
-
-    // Return null if the nonce has not been used
-    // Return $nonce if the nonce was previously used
-    function lookup_nonce($consumer, $token, $nonce, $timestamp)
-    {
-        // Should add some clever logic to keep nonces from
-        // being reused - for no we are really trusting
-        // that the timestamp will save us
-        return null;
-    }
-
-    function new_request_token($consumer)
-    {
-        return null;
-    }
-
-    function new_access_token($token, $consumer)
-    {
-        return null;
+        return new OAuthToken($consumer, '');
     }
 }
