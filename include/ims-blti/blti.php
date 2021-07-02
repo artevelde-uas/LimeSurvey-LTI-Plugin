6ce8439d1233e856e1439d40875f066a03c26797
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2021-07-02 15:02:27 +0200

    Move class `TrivialOAuthDataStore` to seperate file

--- include/ims-blti/blti.php
+++ include/ims-blti/blti.php
@@ -1,11 +1,9 @@
 <?php
 
-use IMSGlobal\LTI\OAuth\OAuthDataStore;
 use IMSGlobal\LTI\OAuth\OAuthServer;
 use IMSGlobal\LTI\OAuth\OAuthSignatureMethod_HMAC_SHA1;
 use IMSGlobal\LTI\OAuth\OAuthRequest;
-use IMSGlobal\LTI\OAuth\OAuthConsumer;
-use IMSGlobal\LTI\OAuth\OAuthToken;
+Use TrivialOAuthDataStore;
 
 
 // Returns true if this is a Basic LTI message
@@ -17,51 +15,6 @@ function is_basic_lti_request() {
    if ($good_message_type and $good_lti_version and isset($resource_link_id) ) return(true);
    return false;
 }
-
-/**
- * A Trivial memory-based store - no support for tokens
- */
-class TrivialOAuthDataStore extends OAuthDataStore {
-    private $consumers = array();
-
-    function add_consumer($consumer_key, $consumer_secret) {
-        $this->consumers[$consumer_key] = $consumer_secret;
-    }
-
-    function lookup_consumer($consumer_key) {
-        if ( strpos($consumer_key, "http://" ) === 0 ) {
-            $consumer = new OAuthConsumer($consumer_key,"secret", NULL);
-            return $consumer;
-        }
-        if ( $this->consumers[$consumer_key] ) {
-            $consumer = new OAuthConsumer($consumer_key,$this->consumers[$consumer_key], NULL);
-            return $consumer;
-        }
-        return NULL;
-    }
-
-    function lookup_token($consumer, $token_type, $token) {
-        return new OAuthToken($consumer, "");
-    }
-
-    // Return NULL if the nonce has not been used
-    // Return $nonce if the nonce was previously used
-    function lookup_nonce($consumer, $token, $nonce, $timestamp) {
-        // Should add some clever logic to keep nonces from
-        // being reused - for no we are really trusting
-        // that the timestamp will save us
-        return NULL;
-    }
-
-    function new_request_token($consumer) {
-        return NULL;
-    }
-
-    function new_access_token($token, $consumer) {
-        return NULL;
-    }
-}
-
 
 // Basic LTI Class that does the setup and provides utility
 // functions
